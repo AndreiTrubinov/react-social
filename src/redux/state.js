b40379ef36eaacf880620861cf99b15f371bb5e2
AndreiTrubinov <hiddengard@gmail.com>
2022-01-12 19:43:53 +0500

    very noob raw proj

---
+++ src/redux/state.js
@@ -0,0 +1,27 @@
+let state = {
+    profilePage: {
+        posts: [
+            { id: 1, message: 'Hi!' },
+            { id: 2, message: 'How are you?' },
+            { id: 3, message: 'When you gonna start to work?' },
+            { id: 4, message: 'Everything will be cool' }
+        ]
+    },
+    dialogsPage: {
+        dialogs: [
+            { id: 1, name: 'Dymych' },
+            { id: 2, name: 'Neo' },
+            { id: 3, name: 'Morpheus' },
+            { id: 4, name: 'Trinity' },
+            { id: 5, name: 'Seraph' },
+        ],
+        messages: [
+            { id: 1, message: 'Yo' },
+            { id: 2, message: 'How is your it Kamasutra?' },
+            { id: 3, message: 'Listen to your heart' },
+            { id: 4, message: 'Follow the white rabbit' },
+        ]
+    }
+}
+
+export default state;
